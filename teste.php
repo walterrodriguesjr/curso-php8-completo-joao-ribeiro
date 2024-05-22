d93c457fbd9a4518d1f24eab3c518f340637b3d0
walterrodriguesjr <walterrjr.86@gmail.com>
2024-05-22 16:26:52 -0300

    atualizando

--- teste.php
+++ teste.php
@@ -1,5 +1,27 @@
 <?php
 
+class Dados
+{
+    private $nome;
+    private $sobreNome;
+    private $cidade;
 
+    public function __construct($a, $b, $c)
+    {
+        $this->nome = $a;
+        $this->sobreNome = $b;
+        $this->cidade = $c;
+    }
 
-?>
+    function coletarDados()
+    {
+        return "Meu nome é $this->nome, meu sobrenome é $this->sobreNome e moro na cidade de $this->cidade";
+    }
+}
+$nome = "Walter";
+$sobrenome = "Rodrigues";
+$cidade = "Curitiba";
+
+$dados = new Dados($nome, $sobrenome, $cidade);
+
+echo $dados->coletarDados();
