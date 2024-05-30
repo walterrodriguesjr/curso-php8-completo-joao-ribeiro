3bd52d6c7a40ec924b42fddda44f9f12db7421d4
walterrodriguesjr <walterrjr.86@gmail.com>
2024-05-30 12:05:46 -0300

    atualizando

--- teste.php
+++ teste.php
@@ -1,5 +1,28 @@
 <?php
 
+class ClasseTeste
+{
+    public $publica = 'a';
+    protected $protegida = 'b';
+    private $privada = 'c';
 
 
-?>
+    function teste()
+    {
+        echo $this->privada . "<br>";
+    }
+
+}
+class ClasseDerivada extends ClasseTeste
+{
+    function testeDerivada()
+    {
+       echo $this->publica;
+       echo $this->protegida;
+       /* echo $this->privada; */
+    }
+}
+
+$a = new ClasseTeste;
+echo $a->teste() . "<br>";
+
